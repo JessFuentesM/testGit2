d1422d69781951c9309e2e7062bfa62530767d28
Jessica <jessicafuentesm@gmail.com>
2017-03-05 15:11:53 +1100

    Initial Commit

---
+++ testGit2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testGit2
+//
+//  Created by Jessica Fuentes on 5/3/17.
+//  Copyright © 2017 Jessica Fuentes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
